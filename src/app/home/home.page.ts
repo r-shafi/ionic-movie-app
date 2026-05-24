2a3ecfeecffb86199fa7903dddd1e8ae9d26b473
Shafi Rayhan <shafirayhan@proton.me>
2026-05-24 20:47:02 +0600

    chore: migrate home page styles to Tailwind

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,6 @@ import { TmdbService } from '../services/tmdb.service';
 @Component({
     selector: 'app-home',
     templateUrl: './home.page.html',
-    styleUrls: ['./home.page.scss'],
     standalone: false
 })
 export class HomePage implements OnInit {
